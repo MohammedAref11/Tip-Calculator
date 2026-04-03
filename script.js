ac90d3634f55033af5732e9b4734808512d57871
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-04-03 18:53:10 +0400

    added event listeners for buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //* user Input fields
 const billInput = document.getElementById("user-bill-input");
-const numberOfPeopleInput = document.getElementById("numOfPeopleInput")
+const numOfPeopleInput = document.getElementById("numOfPeopleInput")
 //* Fetching all select tips buttons in array  
 const selectBtns = document.querySelectorAll("#selectButtons"); 
 //* Reset button
@@ -9,23 +9,28 @@ const resetBtn = document.getElementById("resetBtn");
 const amountEl = document.getElementById("priceAmount");
 const totalEl = document.getElementById("priceTotal");
 
-//* Gets the tips percentage
-function tipPercentage(element) { 
-    element.forEach((item) => { 
-        console.log(item.value)
+
+tipPer = 0; 
+
+selectBtns.forEach((element) => { 
+    element.addEventListener('click', () => { 
+          tipPer = element.value; 
+          console.log(tipPer)
     })
+})
+
+
+function calculateTipAmount() { 
+    const bill = Number(billInput.value);
+    const numOfPeople = Number(numOfPeopleInput.value)
+    const tipAmount = (bill / 100) * tipPer;
+    console.log(tipAmount)
 }
 
-function getUserInput(element) { 
-    const input = Number(element); 
-}
 
+billInput.addEventListener('input', calculateTipAmount)
 
-
-
-
-
-
-
-
-
+resetBtn.addEventListener('click', () => { 
+    billInput.value = ""; 
+    numOfPeopleInput.value = ""
+})
