c8e58607863d7a7be6bd753122da6d642f199d86
MohammedAref11 <245248570+MohammedAref11@users.noreply.github.com>
2026-04-05 20:39:28 +0400

    Finished from adding basics functionality like counting total bill divided by people

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
 //* user Input fields
 const billInput = document.getElementById("user-bill-input");
 const numOfPeopleInput = document.getElementById("numOfPeopleInput")
+// * buttons container
+const buttonsGrid = document.querySelector(".tip-buttons-grid");
 //* Fetching all select tips buttons in array  
-const selectBtns = document.querySelectorAll("#selectButtons"); 
+const tipButtons = document.querySelectorAll("#selectButtons"); 
 //* Reset button
 const resetBtn = document.getElementById("resetBtn"); 
 //* Price elements (Total / Amount)
 const amountEl = document.getElementById("priceAmount");
 const totalEl = document.getElementById("priceTotal");
 
+let billValue;
+let tipPercentage;
 
-tipPer = 0; 
-
-selectBtns.forEach((element) => { 
+tipButtons.forEach(element => { 
     element.addEventListener('click', () => { 
-          tipPer = element.value; 
-          console.log(tipPer)
+        tipPercentage = element.value; 
+        console.log(tipPercentage)
     })
 })
 
+buttonsGrid.addEventListener('click', (event) => {  
 
-function calculateTipAmount() { 
-    const bill = Number(billInput.value);
-    const numOfPeople = Number(numOfPeopleInput.value)
-    const tipAmount = (bill / 100) * tipPer;
-    console.log(tipAmount)
+    tipButtons.forEach(btn => { 
+        if (btn === event.target) { 
+            btn.classList.toggle("clicked")
+        }else { 
+            btn.classList.remove("clicked")
+        }
+    })
+})
+
+function getBillValue() {  
+        const bill = Number(billInput.value);
+        billValue = bill; 
+        console.log(billValue); 
+        displayTotal();
 }
 
-
-billInput.addEventListener('input', calculateTipAmount)
+function displayTotal() { 
+    const numOfPeople = Number(numOfPeopleInput.value); 
+    const totalAmount = billValue / numOfPeople; 
+    totalEl.textContent = `$${totalAmount.toFixed(2)}`; 
+    if (numOfPeople === null || numOfPeople === NaN || numOfPeople === 0) { 
+        totalEl.textContent = `$0.00`; 
+    }
+}
 
 resetBtn.addEventListener('click', () => { 
     billInput.value = ""; 
-    numOfPeopleInput.value = ""
+    numOfPeopleInput.value = ""; 
 })
+
+billInput.addEventListener('input', getBillValue); 
+numOfPeopleInput.addEventListener('input', displayTotal);
